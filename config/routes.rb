437673114ae8b8f32614377dcd6dfcb621c27f82
Guanlong Zhao <guanlongzhao@gmail.com>
2016-10-24 04:23:35 +0000

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+    resources :routes
+    resources :trips
+    resources :stops
+    resources :stop_times
+    resources :calendar_dates
+    resources :calendars
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
