957d06892a6f9d63d23709bd8defc6580c328b7e
Shefali Garg <s.shefaligarg@gmail.com>
2016-10-26 01:54:59 +0000

    buildings schema

--- db/migrate/20161026005539_create_buildings.rb
+++ db/migrate/20161026005539_create_buildings.rb
@@ -1,6 +1,13 @@
 class CreateBuildings < ActiveRecord::Migration[5.0]
   def change
     create_table :buildings do |t|
+      t.integer :number
+      t.string :abbr
+      t.string :name
+      t.string :address
+      t.integer :numberoffloors
+      t.float :lan
+      t.float :lon
     end
   end
 end
