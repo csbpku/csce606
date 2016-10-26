8db90151da2a0054fb93d73ce75fec2a6df15e8e
Guanlong Zhao <guanlongzhao@gmail.com>
2016-10-26 00:12:37 +0000

    Updated routes.rb accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :stop_times
   resources :calendar_dates
   resources :calendars
+  resources :points
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
