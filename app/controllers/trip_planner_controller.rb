ed64908f1a56f6ef67ef54444385337f4a351d98
Xing Zhao <zx_chn@hotmail.com>
2016-11-06 21:30:24 +0000

    update bus route planning framework

--- app/controllers/trip_planner_controller.rb
+++ app/controllers/trip_planner_controller.rb
@@ -2,24 +2,29 @@ require 'geocoder'
 
 
 class TripPlannerController < ApplicationController
+  
   def index
+    
   end
   
   def address_to_coordinates (address)
     #Call geocode and return coordinates
+    return nil
   end
   
   def find_nearest_stop(raw_coordinates)
     #return a nearest station
-  
+    return nil
   end
   
   def walking_route(depart_coordinates, destination_coordinates)
     #return a walking route object
+    return nil
   end
   
   def find_routes_set_by_stop(stop)
     #return a set which contains all routes including input stop
+    return nil
   end
   
   def bus_route_planning (depart_location, destination_location)
@@ -33,16 +38,18 @@ class TripPlannerController < ApplicationController
     destination_walking_route = walking_route(destination_stop.coordinates, destination_coordinates)
     
     #S is the set which contains all routes including the departing stop 
-    S = find_routes_set_by_stop(depart_stop)
+    routes_containing_depart = find_routes_set_by_stop(depart_stop)
     #D is the set which contains all routes including the destination stop 
-    D = find_routes_set_by_stop(destination_stop)
+    routes_containing_destination = find_routes_set_by_stop(destination_stop)
     
-    R = S&D
-    if ( R != nil)
-      for r in R do
-        
+    final_route_set=[]
+    common_routes = routes_containing_depart & routes_containing_depart
+    if ( common_routes != nil)
+      common_routes.each do |common_route|
+          
       end
-    
     else
+      
+    end
   end
 end
