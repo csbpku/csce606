3018bdca81bd3a5ecd944b5770faa3aee4ec1023
Xing Zhao <zx_chn@hotmail.com>
2016-11-06 21:21:27 +0000

    add the framework of bus route planning and fuck hhkb keyboard

--- Gemfile
+++ Gemfile
@@ -142,3 +142,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'rspec', :require => false, :group => :test
 gem 'simplecov', :require => false, :group => :test
+gem 'geocoder'
--- app/controllers/trip_planner_controller.rb
+++ app/controllers/trip_planner_controller.rb
@@ -1,4 +1,48 @@
+require 'geocoder'
+
+
 class TripPlannerController < ApplicationController
   def index
+  end
+  
+  def address_to_coordinates (address)
+    #Call geocode and return coordinates
+  end
+  
+  def find_nearest_stop(raw_coordinates)
+    #return a nearest station
+  
+  end
+  
+  def walking_route(depart_coordinates, destination_coordinates)
+    #return a walking route object
+  end
+  
+  def find_routes_set_by_stop(stop)
+    #return a set which contains all routes including input stop
+  end
+  
+  def bus_route_planning (depart_location, destination_location)
+    depart_coordinates = address_to_coordinates(depart_location)
+    destination_coordinates = address_to_coordinates(destination_location)
+    depart_stop = find_nearest_stop(depart_coordinates)
+    destination_stop = find_nearest_stop(destination_coordinates)
+    
+    
+    depart_walking_route = walking_route(depart_coordinates, depart_stop.coordinates)
+    destination_walking_route = walking_route(destination_stop.coordinates, destination_coordinates)
+    
+    #S is the set which contains all routes including the departing stop 
+    S = find_routes_set_by_stop(depart_stop)
+    #D is the set which contains all routes including the destination stop 
+    D = find_routes_set_by_stop(destination_stop)
+    
+    R = S&D
+    if ( R != nil)
+      for r in R do
+        
+      end
+    
+    else
   end
 end
