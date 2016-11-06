129c8c2eb64a260c91cc213de6c951a27f24df9f
Divyesh <tekale2@tamu.edu>
2016-11-06 23:57:14 +0000

    Added redirect path for brand img

--- app/assets/javascripts/tripPlanner.js
+++ app/assets/javascripts/tripPlanner.js
@@ -1,4 +1,12 @@
+/*tripPlanner.js*/
+// Contains all google api calls
+// Please add any javscript/jquery code
+// in this file
+
+//global variables
 /*global google*/
+
+// This function is first when user loads the page
 function initMap() {
     var tamuCenter = {lat: 30.6187199, lng: -96.3364829};
     var map = new google.maps.Map(document.getElementById('map'), {
