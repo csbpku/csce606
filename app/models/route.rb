e76e66721db2764af0ab8941cfa73dfdd4b7d27a
Guanlong Zhao <guanlongzhao@gmail.com>
2016-11-07 02:46:46 +0000

    Added route.stops association

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,6 @@
 class Route < ActiveRecord::Base
     has_many :trips
-    has_many :shapes, through: :trips
+    has_many :points, through: :trips
+    has_many :stop_times, through: :trips
+    has_many :stops, through: :stop_times
 end
