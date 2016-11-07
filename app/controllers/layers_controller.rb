12554bfd30f7190a454b1ee28c76d56e097ed729
Shefali Garg <s.shefaligarg@gmail.com>
2016-11-07 20:31:43 +0000

    pindrop update

--- app/controllers/layers_controller.rb
+++ app/controllers/layers_controller.rb
@@ -1,7 +1,10 @@
 class LayersController < ApplicationController
     def dropPins
-
-        @buildings = {"objtype"=>"Building","points"=>[{"lan"=>30.6187199,"lon"=>-96.3364829},{"lan"=>30.616842,"lon"=>-96.339240}]}
+        
+        @position = Buildings.all
+        @position = @position.as_json
+       # @buildings = {"objtype"=>"Building","points"=>[{"lan"=>30.6187199,"lon"=>-96.3364829},{"lan"=>30.616842,"lon"=>-96.339240}]}
+        @buildings = {"objtype"=>"Building","points"=> @position}
         if params["layer"]["building"]
              render json: @buildings
         end
