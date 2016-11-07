8a8327c389efac963ff546b7eb1dc301f9cfaa49
Guanlong Zhao <guanlongzhao@gmail.com>
2016-11-07 21:59:55 +0000

    Added some test code

--- spec/controllers/trip_planner_controller_spec.rb
+++ spec/controllers/trip_planner_controller_spec.rb
@@ -1,13 +1,19 @@
+require "rails_helper"
 
-describe 'POST #create' do
-  context 'when source and destination are valid' do
-    # it 'renders the page with error' do
-      # user = create(:user)
-
-      # post :create, session: { email: user.email, password: 'invalid' }
-
-      # expect(response).to render_template(:new)
-      # expect(flash[:notice]).to match(/^Email and password do not match/)
+RSpec.describe TripPlannerController, :type => :controller  do
+  describe "Get coordinate from address" do
+    it "Sucessfully converts a string address to geolocations" do
+      expect(controller.address_to_coordinates("HRBB")).to eq([30.6189722, -96.3387678])
     end
   end
+  
+  describe "Find the nearest bus stop given a location" do
+    it "Finds the nearest stop" do
+      expect(controller.find_nearest_stop([30.6189722, -96.3387678]).code).to eq(416)
+    end
+  end
+  
+  describe "Find the walking path from one palce to another" do
+    it "Finds the walking path" do
+      
 end
