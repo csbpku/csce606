2884179d7e0591d98079e6cb6176bcffd880a3b7
Shefali Garg <s.shefaligarg@gmail.com>
2016-11-08 23:49:56 +0000

    building model update

--- spec/controllers/layers_controller_spec.rb
+++ spec/controllers/layers_controller_spec.rb
@@ -1,4 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe LayersController, type: :controller do
+    describe "Get data for buildings" do
+        it "Successfully return building object if params exist" do
+            get :dropPins, {:layer => {:building => '1'}}
+            expect(response.body).to eq("{\"layer\":[{\"objtype\":\"Building\",\"points\":[]}]}") 
+        end
+    end
+    
+    describe "Get data for car parking" do
+        it "Successfully return car parking object if params exist" do
+            get :dropPins, {:layer => {:parking => '1'}}
+            expect(response.body).to eq("{\"layer\":[{\"objtype\":\"Parking\",\"points\":[]}]}") 
+        end
+    end
+    
+    describe "Get data for bicycle parking" do
+        it "Successfully return bicycle parking object if params exist" do
+            get :dropPins, {:layer => {:bicycle => '1'}}
+            expect(response.body).to eq("{\"layer\":[{\"objtype\":\"Bikes\",\"points\":[]}]}") 
+        end
+    end
+
 end
