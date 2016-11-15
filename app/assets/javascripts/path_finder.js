7b8ba95935da2d65c1010c5e3c8ad5d34dd8b31b
shefaligarg90 <s.shefaligarg@gmail.com>
2016-11-15 04:05:32 +0000

    close dialog on click

--- app/assets/javascripts/path_finder.js
+++ app/assets/javascripts/path_finder.js
@@ -1,4 +1,4 @@
-/*tripPlanner.js*/
+/*path_finder.js*/
 // Contains all google api calls
 // Please add any javscript/jquery code
 // in this file
@@ -9,41 +9,8 @@
 /*global google*/
 /*global $*/
 var map;
-var icons = {
-  Building: {
-    icon: 'https://maps.google.com/mapfiles/kml/paddle/orange-stars.png'
-  },
-  Parking: {
-    icon: 'https://maps.google.com/mapfiles/kml/paddle/blu-diamond.png'
-  },
-  Bikes: {
-    icon: 'https://maps.google.com/mapfiles/kml/paddle/B.png'
-  },
- 
-};
-
-
 
 /*-----------------------------Functions-----------------------------------------*/
-
-function popupcontent(type, point) {
- switch (type){
-    case 'Building':
-      return '<div><strong>' + point.name + '</strong><br>' +
-             'Address: ' + point.address + '<br>' +
-              point.lat + ', '+ + point.lon + '<br>' + '</div>';
-
-    case 'Parking':
-      return '<div><strong>' + point.lotname + '</strong><br>' +
-             'Type: ' + point.lottype + '<br>' + 
-             point.lat + ', '+ + point.lon + '<br>' + '</div>';
-    case 'Bikes':
-      /*
-        Write your bike rails code here
-      */
-  }
-}
-
 // This function is first when user loads the page
 function initMap() {
     var tamuCenter = {lat: 30.6187199, lng: -96.3364829};
@@ -57,48 +24,11 @@ function initMap() {
     });
 }
 
-// This function is to create pop up
-function createpopup(marker,contentString) {
-    var infowindow = new google.maps.InfoWindow({
-          content: contentString
-    });
-    
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-    });
-}
-
-// Creates markers on the map
-function createMarker(latlng, type, point){
-    var marker = new google.maps.Marker({
-      map: map,
-      position: latlng,
-      title: type,
-      icon: {url: icons[type].icon, scaledSize: new google.maps.Size(20,20)}
-    });
-    var contentString = popupcontent(type, point);
-    createpopup(marker,contentString);
-}
-
-function displayMarkers(data)
-{
-    console.log(data);//for debuging only : temporary
-    
-    for(var j = 0;j<data.layer.length;j++){
-        for (var i = 0; i < data.layer[j].points.length; i++){
-            var latlng = new google.maps.LatLng(data.layer[j].points[i].lat, data.layer[j].points[i].lon);
-            createMarker(latlng, data.layer[j].objtype,data.layer[j].points[i]);
-        }
-    }
-}
-
 // Layer populater
 $(document).ready(function() {
     
     $('#pathFinderForm').submit(function(event) {
-        console.log("i AM HERE");
         $('#path_finder').modal('hide');
-        
         // stop the form from submitting the normal way and refreshing the page
         event.preventDefault();
     });
