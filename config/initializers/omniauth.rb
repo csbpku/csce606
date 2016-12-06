0279b2a9af3977593fbbc80dbe1b077f053b329b
mragank <mragank@tamu.edu>
2016-12-06 09:53:43 -0600

    Google Auth Login
    Backend changes to allow google auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'S62K0c26t1ADWOZ3TuCrTsPDt', 'xJhwIsz8xjjcggVuCdcBuNo2ZUkbK9Ebj0yiJB1HNWy97d9sZp'
+  provider :google_oauth2, '1085946968742-5da5f998m0glstltqgjo3p1a3iau0e27.apps.googleusercontent.com', 'C5egbBfZPjkHnF8pK9X5jnzb', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
