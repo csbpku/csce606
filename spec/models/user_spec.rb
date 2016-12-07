253f933b11ab7ee50aaf8b8d58cb41762783f6ef
shefaligarg90 <s.shefaligarg@gmail.com>
2016-12-07 01:05:50 +0000

    update model provider

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe User do
         result = User.from_omniauth(dummy)
         expect(result.uid).to eq("1")
     end
+    
+    it 'Return url of selected provider' do
+        dummy = {'uid' => 1, 'provider' => 'linkedin', 'info' => {'name' => 'xyz', 'location' => 'abs', 'image' =>'', 'urls' => {'public_profile' => 'www'}}}
+        result = User.get_social_url_for('linkedin', dummy['info']['urls'])
+        expect(result).to eq(nil)
+    end
+    
 end
