8f0e1b1b4fe3bcbe006f3fa73fc38535a5f97d2d
kritikakurani <kritikakurani@tamu.edu>
2016-12-06 21:24:09 -0600

    Texas Logo Changes
    Texas Logo Changes

--- features/step_definitions/texaslogo_steps.rb
+++ features/step_definitions/texaslogo_steps.rb
@@ -3,9 +3,9 @@ Given(/^I am at the Trip Planning Home page$/) do
 end
 
 When(/^I search for Logo$/) do
-  find('.navbar-brand').click
+  find('.container-fluid .navbar-brand').click
 end
 
 Then(/^I should get the existence of logo button$/) do
-   expect(page).to have_css('.navbar-brand')
+   expect(page).to have_css('.container-fluid .navbar-brand')
 end
