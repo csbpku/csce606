ac9825470b1a6abf5c57cd30a3e3d83281bd42bd
shefaligarg90 <s.shefaligarg@gmail.com>
2016-12-07 02:25:09 +0000

    update session creation with no request

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController do
+    describe "create session for user with no response" do
+        it "Successfully create a session for user once login is completed with no response" do
+            get :create
+            expect(response).to redirect_to(root_path)
+        end
+    end
 end
